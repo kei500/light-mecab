0af4296d66f7e2f3792c953b68679f8570c85eff
Kei Tsuchiya <kei.tsuchiya86@gmail.com>
2013-10-04 03:23:31 +0900

    First version

--- lib/mecablight.rb
+++ lib/mecablight.rb
@@ -1,5 +1,63 @@
-require "mecablight/version"
+require 'mecablight/version'
+require 'MeCab'
+require 'yaml'
 
 module Mecablight
-  # Your code goes here...
+  class Morpheme
+    @@i18n = ::YAML.load_file(File.expand_path(File.join(__FILE__, '..', 'locale', 'morpheme.yml')))
+
+    class << self
+      # @param text [String]
+      # @return [Array <MeCab::Node>] 
+      def analyze(text)
+        nodes = []
+        node = ::MeCab::Tagger.new.parseToNode(text)
+        while node
+          nodes << node
+          node = node.next
+        end
+        nodes.shift
+        nodes.pop
+        nodes
+      end
+
+      # @param key [String or Symbol]
+      # @return [String]
+      def i18n(key)
+        key.is_a?(Symbol) ? @@i18n[key.to_s] : @@i18n[key]
+      end
+    end
+
+    # @param text [String]
+    def initialize(text)
+      @nodes = self.class.analyze(text)
+    end
+
+    # @return [Integer]
+    def count
+      @nodes.size
+    end
+
+    # @param method_name [Symbol]
+    def method_missing(method_name)
+      if !self.class.i18n(method_name)
+        raise NoMethodError
+      end
+      extract(self.class.i18n(method_name))
+    end
+
+    private
+
+    # @param name [String]
+    # @return [Array <String>]
+    def extract(name)
+      morpheme = []
+      @nodes.each do |node|
+        if name == node.feature.split(',').first.force_encoding('UTF-8')
+          morpheme << node.surface.force_encoding('UTF-8')
+        end
+      end
+      morpheme
+    end
+  end
 end
--- mecablight.gemspec
+++ mecablight.gemspec
@@ -4,20 +4,17 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'mecablight/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "mecablight"
+  spec.name          = 'mecab-light'
   spec.version       = Mecablight::VERSION
-  spec.authors       = ["Kei Tsuchiya"]
-  spec.email         = ["kei.tsuchiya86@gmail.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
-  spec.homepage      = ""
-  spec.license       = "MIT"
+  spec.authors       = ['Kei Tsuchiya']
+  spec.email         = ['kei.tsuchiya86@gmail.com']
+  spec.description   = %q{Execute morphological analysis by MeCab}
+  spec.summary       = %q{Wrapping mecab-ruby to execute morphological analysis easlily}
+  spec.homepage      = 'https://github.com/kei500/mecab-light'
+  spec.license       = 'MIT'
 
   spec.files         = `git ls-files`.split($/)
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = ["lib"]
-
-  spec.add_development_dependency "bundler", "~> 1.3"
-  spec.add_development_dependency "rake"
+  spec.require_paths = ['lib']
 end
